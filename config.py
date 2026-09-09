00d56889de8e7ed621bbfcbcbacd66273e707bc5
FialhoMath <mgfm@cesar.school>
2026-09-09 15:16:14 -0300

    payload max add nas constantes

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ PORTA= 8082
 JANELA_DEFAULT= 5
 TAM_TEXTO_MIN = 30
 BUFFER_SIZE = 1024 #valor de bytes maximo que a funcao recvfrom puxa para receber pacote
+PAYLOAD_MAX = 4 #maximo de caracteres uteis por pacote, usado na fragmentacao a partir do CP2
+
 
 ENDERECO_SERVIDOR = (IP, PORTA)
